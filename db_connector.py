f18885733352536cd643b9e8a6cf906d80cf8610
Robin Reinhardt <r0b1nr31nh4rdt@gmail.com>
2026-03-12 16:55:53 +0100

    schema and connector created

---
+++ db_connector.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import sqlite3
+
+PATH_DB = "./my_own_cve.db"
+PATH_SQL = "./schema.sql"
+
+def init_db(path_db, path_sql):
+    if Path.exists(path_sql):
+        try:
+            with open(path_sql, 'r') as f:
+                sql_script = f.read()
+                print(sql_script)
+                conn = sqlite3.connect(path_db)
+                cursor = conn.cursor()
+                cursor.executescript(sql_script)
+                conn.commit()
+                return conn
+        except:
+            raise Exception ("Error while creating DB")
+    else:
+        raise Exception ("No schema.sql found, cant't create DB")
+
+def open_db(path):
+    try:
+        conn = sqlite3.connect(path)
+        conn.execute("PRAGMA foreign_keys = ON")
+        return conn
+    except:
+        raise Exception ("Failed to open DB")
+
+def start():
+    if Path.exists(PATH_DB):
+        conn = open_db(PATH_DB)
+    else:
+        conn = init_db(PATH_DB, PATH_SQL)
+    conn.execute("PRAGMA foreign_keys = ON")
+    return conn
---
+++ main.py
@@ -0,0 +1,7 @@
+from db_connector import start
+
+# conn = start()
+
+
+if __name__ == '__main__':
+    conn = start()
